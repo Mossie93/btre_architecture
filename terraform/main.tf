b30606658d5c2da283e4ceadddab68f0c9e5b4ad
Rafal Mossakowski <rmossakowski@gmail.com>
2020-04-26 20:03:25 +0200

    Add droplet

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,30 @@ variable "digitalocean_token" {
   description = "Your digitalocean API token"
 }
 
+variable "ssh_fingerprints" {
+  type = list(string)
+  description = "List of SSH keys fingerprints"
+}
+
 provider "digitalocean" {
   token = var.digitalocean_token
+}
+
+resource "digitalocean_tag" "webserver" {
+  name = "web"
+}
+
+resource "digitalocean_tag" "env" {
+  name = "production"
+}
+
+resource "digitalocean_droplet" "webserver" {
+  name = "webserver"
+  image = "ubuntu-18-04-x64"
+  size = "1gb"
+  region = "fra1"
+  ipv6 = true
+  private_networking = false
+  tags = [digitalocean_tag.webserver.id, digitalocean_tag.env.id]
+  ssh_keys = var.ssh_fingerprints
 }
