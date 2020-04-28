153ba080d844ccdb66dff738c43d55c732ac0244
Rafal Mossakowski <rmossakowski@gmail.com>
2020-04-28 14:53:49 +0200

    Specifyaddresses in DO firewalls

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,26 +38,31 @@ resource "digitalocean_firewall" "webserver" {
   inbound_rule {
     protocol = "tcp"
     port_range = "22"
+    source_addresses = ["0.0.0.0/0"]
   }
 
   inbound_rule {
     protocol = "tcp"
     port_range = "80"
+    source_addresses = ["0.0.0.0/0"]
   }
 
   inbound_rule {
     protocol = "tcp"
     port_range = "443"
+    source_addresses = ["0.0.0.0/0"]
   }
 
   outbound_rule {
     protocol = "tcp"
     port_range = "53"
+    destination_addresses = ["0.0.0.0/0"]
   }
 
   outbound_rule {
     protocol = "udp"
     port_range = "53"
+    destination_addresses = ["0.0.0.0/0"]
   }
 }
 
